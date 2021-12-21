3e1bfdf45c9715f21027ef079a2c4be1d43498cd
Nicholas Shephard <shephardn@gmail.com>
2021-12-21 08:31:42 -0600

    commit tree 1634bdd403a088e76733a1b7074cca7299499f88

--- db/migrate/20211221130909_create_reviews.rb
+++ db/migrate/20211221130909_create_reviews.rb
@@ -3,7 +3,9 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.integer :score
       t.string :comment
-      t.integer :game_id # this is our foreign key
+      t.integer :game_id
+      # t.belongs_to :game 
+      #    >> and in the corresponding model --> belongs_to :game
       t.timestamps
     end
   end
